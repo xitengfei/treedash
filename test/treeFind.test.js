e5825e2547fa5598ab8e01c4ae5c8162ea9ba655
Tengfei Xi <xitengfei@github.com>
2022-01-18 16:51:19 +0800

    treefind support DFS

--- test/treeFind.test.js
+++ test/treeFind.test.js
@@ -1,11 +1,78 @@
-import {treeFind} from '../src';
+import { treeFind } from '../src';
 import OldTreeLip from './old/Tree';
 import areas from './json/testCase.json';
 
+
+const treeData = [
+  {
+    "code": "130000",
+    "name": "河北省",
+    "children": [
+      {
+        "code": "130100",
+        "name": "石家庄",
+        "children": [
+          {
+            "code": "130102",
+            "name": "长安区",
+          },
+          {
+            "code": "130103",
+            "name": "桥东区",
+          },
+          {
+            "code": "130133",
+            "name": "赵县",
+            "level": 2,
+            "isLeaf": true,
+            "path": "0,130000,130100"
+          },
+          {
+            "code": "130181",
+            "name": "辛集市",
+            "level": 2,
+            "isLeaf": true,
+            "path": "0,130000,130100"
+          },
+        ]
+      },
+      {
+        "code": "130200",
+        "name": "唐山市",
+        "children": [
+          {
+            "code": "130202",
+            "name": "路南区"
+          }
+        ]
+      }
+    ]
+  }
+];
+
 describe('treeFind Test: ', () => {
   test('general case', () => {
-    const iterator = item => item.name === '顺义区';
-    const expected = OldTreeLip.treeFindNode(areas, iterator);
-    expect(treeFind(areas, iterator)).toEqual(expected);
+    const expected = {
+      "code": "130200",
+      "name": "唐山市",
+      "children": [
+        {
+          "code": "130202",
+          "name": "路南区"
+        }
+      ]
+    };
+    expect(treeFind(treeData, item => item.name.endsWith('市'), {algorithm: 'BFS'})).toEqual(expected);
+  });
+
+  test('DFS algorithm case: ', () => {
+    const expected = {
+      "code": "130181",
+      "name": "辛集市",
+      "level": 2,
+      "isLeaf": true,
+      "path": "0,130000,130100"
+    };
+    expect(treeFind(treeData, item => item.name.endsWith('市'), {algorithm: 'DFS'})).toEqual(expected);
   });
 });

c2096d01dd95cf894802100b549be618e57d258d
李凤宝 <lifb@glodon.com>
2022-06-13 16:47:50 +0800

    Add optional iterator to treeBind

--- src/libs/treeBuild/index.ts
+++ src/libs/treeBuild/index.ts
@@ -30,11 +30,29 @@ export type TreeNode<T = unknown> = T & {
   path: string;
 };
 
-const treeBuild = function <T extends AnyObj>(
+export default function treeBuild<T extends AnyObj, R = T>(
   dataSource: T[],
-  options: buildTreeOptions<T> = {}
-): TreeNode<T>[] {
+  iterator?: (node: T, parent?: T) => R | false
+): TreeNode<R>[];
+export default function treeBuild<T extends AnyObj, R = T>(
+  dataSource: T[],
+  options?: buildTreeOptions<T>
+): TreeNode<R>[];
+export default function treeBuild<T extends AnyObj, R = T>(
+  dataSource: T[],
+  iterator?: (node: T, parent?: T) => R | false,
+  options?: buildTreeOptions<T>
+): TreeNode<R>[];
+export default function treeBuild<T extends AnyObj, R = T>(
+  dataSource: T[],
+  iterator?: ((node: T, parent?: T) => R | false) | buildTreeOptions<T>,
+  options?: buildTreeOptions<T>
+): TreeNode<R>[] {
   if (!dataSource || !dataSource.length) return [];
+  if (typeof iterator !== "function" && options === undefined) {
+    options = iterator as any;
+    iterator = undefined;
+  }
 
   const {
     idKey = "id",
@@ -42,11 +60,11 @@ const treeBuild = function <T extends AnyObj>(
     childKey = "children",
     rootPid = 0,
     getChilds,
-  } = options;
+  } = options || {};
 
   // let counter = 0;
 
-  const loop = function (pid: any, level: number, path: string): TreeNode<T>[] {
+  const loop = function (pid: any, level: number, path: string): TreeNode<R>[] {
     // counter++;
 
     // get childs
@@ -57,20 +75,40 @@ const treeBuild = function <T extends AnyObj>(
       nodes = dataSource.filter((item) => item[parentIdKey] === pid);
     }
 
-    return nodes.map((node) => {
-      const children = loop(node[idKey], level + 1, path + "," + node[idKey]);
-      return {
-        ...node,
-        level,
-        isLeaf: !children.length,
-        path,
-        [childKey]: children || [],
-      };
-    });
+    return nodes
+      .map((node) => {
+        const children = loop(node[idKey], level + 1, path + "," + node[idKey]);
+        if (iterator) {
+          const result = (iterator as iterator<T, R>)(
+            node,
+            dataSource.filter((item) => item[idKey] === pid)[0]
+          );
+          if (result === false) {
+            return undefined!;
+          } else {
+            return {
+              level,
+              isLeaf: !children.length,
+              path,
+              ...result,
+              [childKey]: children || [],
+            } as TreeNode<R>;
+          }
+        } else {
+          return ({
+            ...node,
+            level,
+            isLeaf: !children.length,
+            path,
+            [childKey]: children || [],
+          } as unknown) as TreeNode<R>;
+        }
+      })
+      .filter(Boolean);
   };
 
   // console.log('loop count:', counter, dataSource.length);
   return loop(rootPid, 0, rootPid);
-};
+}
 
-export default treeBuild;
+export type iterator<T, R = T> = (node: T, parent?: T) => R | false;

4c5d53fcdbd462c2c5027ca7ddf16895ae039b68
Tengfei Xi <xitf@glodon.com>
2022-07-06 10:24:04 +0800

    add some description

--- src/libs/treeSearch/index.ts
+++ src/libs/treeSearch/index.ts
@@ -1,42 +1,41 @@
-import {
-  IsMatch,
-  AnyObj,
-  BaseOptions
-} from '../interfaces';
-import {isTypeOf} from '../../utils';
+import { IsMatch, AnyObj, BaseOptions } from "../interfaces";
+import { isTypeOf } from "../../utils";
 
 /**
  * Search In Data Tree（返回完整树结构）
- * @param treeData 
- * @param isMatch 
- * @param options 
- * @returns 
+ * @param treeData
+ * @param isMatch
+ * @param options
+ * @returns
  */
- const treeSearch = function<T extends AnyObj>(treeData: Array<T>, isMatch: IsMatch<T>, options: BaseOptions = {}): Array<T> {
+const treeSearch = function <T extends AnyObj>(
+  treeData: Array<T>,
+  isMatch: IsMatch<T>,
+  options: BaseOptions = {}
+): Array<T> {
   // check params
-  if (!isTypeOf(treeData, 'array')) return [];
-  if (!isTypeOf(isMatch, 'function')) return [];
+  if (!isTypeOf(treeData, "array")) return [];
+  if (!isTypeOf(isMatch, "function")) return [];
 
-  const {
-    childKey = 'children'
-  } = options;
+  const { childKey = "children" } = options;
 
   const loop = function (nodes: Array<T>) {
     let matchNodes: Array<T> = [];
 
-    nodes.forEach(node => {
+    nodes.forEach((node) => {
       let childs: Array<T> = [];
-      if (node[childKey] && node[childKey].length){
+      if (node[childKey] && node[childKey].length) {
         childs = loop(node[childKey]);
       }
 
       if (childs.length) {
         // add matched childs only
-        const newNode = {...node, [childKey]: childs };
+        const newNode = { ...node, [childKey]: childs };
         matchNodes.push(newNode);
-      } else if(isMatch(node)){
-        let newNode = {...node};
-        delete newNode[childKey]
+      } else if (isMatch(node)) {
+        // 这里如果子节点中已经有匹配了，则该节点本身就已经属于要返回的结果集了，因此不用再做判断，如果子节点没有匹配，才需要判断该节点本身是否匹配
+        let newNode = { ...node };
+        delete newNode[childKey];
         matchNodes.push(newNode);
       }
     });
